7947962a47ec883292201765cba6c14219bac38b
Sam Goldman <samwgoldman@gmail.com>
2012-07-22 03:06:16 -0400

    Simplify Rack client spec.

--- spec/clients/rack_spec.rb
+++ spec/clients/rack_spec.rb
@@ -4,10 +4,8 @@ require "http_spec/clients/rack"
 describe HTTPSpec::Clients::Rack do
   def app
     lambda do |env|
-      request_body = env["rack.input"].read
-      response_body = env.map { |k, v| "#{k}: #{v}\n" }
-      response_body << "rack.input: #{request_body}"
-      [200, {"X-Foo" => "Bar"}, response_body]
+      @env = env
+      [200, { "Foo" => "Bar" }, ["hello"]]
     end
   end
 
@@ -15,45 +13,41 @@ describe HTTPSpec::Clients::Rack do
 
   it "issues requests to the app" do
     request = HTTPSpec::Request.new(:get, "/path")
-    response = client.dispatch(request)
-    response.body.should match(/^REQUEST_METHOD: GET$/)
-    response.body.should match(/^PATH_INFO: \/path$/)
+    client.dispatch(request)
+    @env["REQUEST_METHOD"].should eq("GET")
+    @env["PATH_INFO"].should eq("/path")
   end
 
   it "accepts query parameters as part of the path" do
     request = HTTPSpec::Request.new(:get, "/path?query=string")
-    response = client.dispatch(request)
-    response.body.should match(/^QUERY_STRING: query=string$/)
+    client.dispatch(request)
+    @env["QUERY_STRING"].should eq("query=string")
   end
 
   it "sends the response body as input" do
     request = HTTPSpec::Request.new(:post, "/path", "foobarbaz")
-    response = client.dispatch(request)
-    response.body.should match(/^rack.input: foobarbaz$/)
+    client.dispatch(request)
+    @env["rack.input"].read.should eq("foobarbaz")
   end
 
   it "converts headers to env before requesting" do
     headers = {
       "Content-Type" => "x-foo-bar",
       "Content-Length" => "10",
-      "X-Foo" => "Bar"
+      "Foo" => "Bar"
     }
     request = HTTPSpec::Request.new(:get, "/path", nil, headers)
     response = client.dispatch(request)
-    response.body.should match(/^CONTENT_TYPE: x-foo-bar$/)
-    response.body.should match(/^CONTENT_LENGTH: 10$/)
-    response.body.should match(/^HTTP_X_FOO: Bar$/)
+    @env["CONTENT_TYPE"].should eq("x-foo-bar")
+    @env["CONTENT_LENGTH"].should eq("10")
+    @env["HTTP_FOO"].should eq("Bar")
   end
 
-  it "exposes the response status" do
+  it "returns the response" do
     request = HTTPSpec::Request.new(:get, "/path")
     response = client.dispatch(request)
     response.status.should eq(200)
-  end
-
-  it "exposes the response headers" do
-    request = HTTPSpec::Request.new(:get, "/path")
-    response = client.dispatch(request)
-    response.headers["X-Foo"].should eq("Bar")
+    response.headers["Foo"].should eq("Bar")
+    response.body.should eq("hello")
   end
 end
