c881f50b977aab965023c0ae2a8cc4c31478080b
Sam Goldman <samwgoldman@gmail.com>
2012-08-10 23:28:03 +0200

    Bump aruba timeout to survive jruby load times in CI.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,10 @@ require "aruba/cucumber"
 require "capybara"
 require "raddocs"
 
+Before do
+  @aruba_timeout_seconds = 5
+end
+
 Raddocs.configure do |config|
   config.docs_dir = "tmp/aruba/docs"
 end
